f021698590392e8024150b7a00e211fe45f4ce0e
yuanzhng <yzhuang@ucls.uchicago.edu>
2022-04-05 15:06:54 -0500

    ignore stuffs

--- src/main/java/com/example/ja/HelloController.java
+++ src/main/java/com/example/ja/HelloController.java
@@ -2,14 +2,60 @@ package com.example.ja;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
-    
+    private Button btnadd;
+    @FXML
+    private Button btnsub;
+    @FXML
+    private Button btnmul;
+    @FXML
+    private Button btndiv;
+    @FXML
+    private Button btnclr;
+    @FXML
+    private TextField txtnum1;
+    @FXML
+    private TextField txtnum2;
+    @FXML
+    private Label lblans;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to farvis Application!");
+    protected void handleButtonAction(ActionEvent e) {
+        if (e.getSource() == btnclr) {
+            txtnum1.setText("");
+            txtnum2.setText("");
+            lblans.setText("?");
+            txtnum1.requestFocus();
+            return;
+        }
+
+        int num1 = Integer.parseInt(txtnum1.getText());
+        int num2 = Integer.(txtnum2.getText());
+        int answer;
+        String symbol = null;
+
+        if (e.getSource() == btnadd) {
+            symbol = "+";
+            answer = num1 + num2;
+        }
+        else if (e.getSource() == btnsub) {
+            symbol = "-";
+            answer = num1 - num2;
+        }
+        else if (e.getSource() == btnmul) {
+            symbol = "*";
+            answer = num1 * num2;
+        }
+        else if (e.getSource() == btndiv) {
+            symbol = "/";
+            answer = num1 / num2;
+        }
+
+        lblans.setText("" + num1 + " " + symbol + " " + num2 + " " + "=" + " " + answer);
     }
 }
